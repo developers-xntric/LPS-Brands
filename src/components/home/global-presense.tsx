79db1015faef4a8fc4da261c8f0b7592b29cfa61
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-13 12:11:37 +0500

    global presence award data

--- src/components/home/global-presense.tsx
+++ src/components/home/global-presense.tsx
@@ -2,11 +2,37 @@ import React from 'react'
 import Wrapper from '../layout/wrapper'
 import Image from 'next/image'
 
+const data = [{
+    image: "/home/a1.webp",
+    heading: "2024",
+    text: "Campaign Middle East Influencer Management Team of the Year"
+}, {
+    image: "/home/a2.webp",
+    heading: "2023",
+    text: "Shortlisted amongst the Top Five Social Media Agencies in the globe; the sole MENA agency to be included in the category"
+}, {
+    image: "/home/a3.webp",
+    heading: "2023",
+    text: "Social content partners for Visit Dubai; Created global virality for the brand with CGI video storytelling"
+}, {
+    image: "/home/a4.webp",
+    heading: "2023",
+    text: "Registered Successes for strategic brand organizations like Dubai Chambers, Liva Insurance, Dubai SIlicon Oasis"
+}, {
+    image: "/home/a5.webp",
+    heading: "2023",
+    text: "An award-winning video production team from idea to execution to last mile adaptations; with experience across BtoC and BtoB sectors"
+}, {
+    image: "/home/a6.webp",
+    heading: "2022",
+    text: "Built products for new age platforms - game streaming, short form videos and live commerce with successful brand adoption"
+}]
+
 const GlobalPresence = () => {
     return (
         <Wrapper>
-            <div className='flex h-screen py-16'>
-                <div className='relative w-[65%] bg-[url("/home/bg-grad.png")] bg-cover bg-center rounded-3xl'>
+            <div className='flex justify-between py-16'>
+                <div className='relative w-[61%] bg-[url("/home/bg-grad.png")] bg-cover bg-center h-[650px] rounded-3xl'>
                     <Image src={'/home/zig-zag.png'} alt='' width={200} height={200} className='absolute top-10 -right-14' />
                     <div className='relative w-[85%] mx-auto h-[65%] mt-20 flex gap-10'>
                         <div className='w-[200px]'>
@@ -30,7 +56,9 @@ const GlobalPresence = () => {
                         </div>
                     </div>
                 </div>
-                <div></div>
+                <div className='w-[32%]'>
+                    <h2 className='text-center text-5xl font-["Exo"] font-[700]'>Awards</h2>
+                </div>
             </div>
         </Wrapper>
     )
