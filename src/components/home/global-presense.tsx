ecf6f74d2455cee71fb89abf6470a01f8d372049
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-13 13:03:06 +0500

    custom width height assigned

--- src/components/home/global-presense.tsx
+++ src/components/home/global-presense.tsx
@@ -23,10 +23,10 @@ const GlobalPresence = () => {
                                 <Image src={'/home/Egypt.webp'} alt='Cities' width={165} height={150} className='inline-block w-[170px]' />
                                 <Image src={'/home/Canada.webp'} alt='Cities' width={220} height={250} className='inline-block w-[235px] relative top-[50px]' />
                             </div>
-                            <div className='relative right-[26%]'>
-                                <Image src={'/home/Saudi.webp'} alt='Cities' width={190} height={200} className='inline-block relative bottom-[70px]' />
-                                <Image src={'/home/UAE.webp'} alt='Cities' width={275} height={250} className='inline-block' />
-                                <Image src={'/home/UK.webp'} alt='Cities' width={185} height={200} className='inline-block relative bottom-[66px]' />
+                            <div className='relative right-[26%] 2xl:right-[19.75%]'>
+                                <Image src={'/home/Saudi.webp'} alt='Cities' width={190} height={200} className='inline-block w-[190px] relative bottom-[70px] 2xl:bottom-[75px]' />
+                                <Image src={'/home/UAE.webp'} alt='Cities' width={275} height={250} className='inline-block w-[290px]' />
+                                <Image src={'/home/UK.webp'} alt='Cities' width={185} height={200} className='inline-block relative bottom-[66px] w-[210px]' />
                             </div>
                         </div>
                     </div>
