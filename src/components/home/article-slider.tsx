d9feda01b16321417fdc3b222155300813797fb7
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-15 11:54:20 +0500

    Data-Difference

--- src/components/home/article-slider.tsx
+++ src/components/home/article-slider.tsx
@@ -5,46 +5,7 @@ import type React from "react"
 import { Button } from "@/components/ui/button"
 import { useEffect, useRef, useState } from "react"
 import Wrapper from "../layout/wrapper"
-
-interface Article {
-    id: number
-    title: string
-    image: string
-    description?: string
-}
-
-const articles: Article[] = [
-    {
-        id: 1,
-        title: "From Design to Deployment: The UX Principles that Drive Engagement in MENA",
-        image: "/article/a1.webp",
-    },
-    {
-        id: 2,
-        title: "What Should You Look Out for While Searching for the Best Creative Agency in Dubai?",
-        image: "/article/a2.webp",
-    },
-    {
-        id: 3,
-        title: "Content Alone Isn’t Enough: Are GCC Businesses Leveraging Social Media Enough?",
-        image: "/article/a3.webp",
-    },
-    {
-        id: 4,
-        title: "Ajmal Perfumes narrates ‘The Untold Stories’ with celebrity perfumers",
-        image: "/article/a4.webp",
-    },
-    {
-        id: 5,
-        title: "The death knell for subjectivity: Data breeds insights",
-        image: "/article/a5.webp",
-    },
-    {
-        id: 6,
-        title: "LPS wins Wasl Group’s social media and content mandate",
-        image: "/article/a6.webp",
-    },
-]
+import { articles } from "@/data/article-slider"
 
 export function ArticlesCarousel() {
     const [currentIndex, setCurrentIndex] = useState(0)
