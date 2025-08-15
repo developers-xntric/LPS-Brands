8d28b784ddefdfe6d661563ee457313788ae8632
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-15 16:11:38 +0500

    Update

--- src/components/home/blog-section.tsx
+++ src/components/home/blog-section.tsx
@@ -8,7 +8,7 @@ import InstagramVideos from "./instagram-videos"
 export default function BlogsSection() {
     return (
         <Wrapper>
-            <div className="max-w-7xl mx-auto bg-[url('/home/blog-bg.png')] rounded-3xl bg-center bg-cover bg-no-repeat">
+            <div className="py-16 px-12 mx-auto bg-[url('/home/blog-bg.png')] rounded-3xl bg-center bg-cover bg-no-repeat">
                 {/* Header */}
                 <div className="flex items-center justify-between mb-12">
                     <h1 className="text-4xl font-bold text-white">Blogs</h1>
