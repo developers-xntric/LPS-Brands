063e70fcf13e8b6f3421c17485cfba13dfb39dd3
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-18 12:55:47 +0500

    leadership Hero

--- src/app/(screens)/services/leadership-branding/page.tsx
+++ src/app/(screens)/services/leadership-branding/page.tsx
@@ -1,9 +1,10 @@
+import CareersHero from '@/components/careers/careers-hero';
 import React from 'react'
 
 const LeadershipBranding = () => {
     return (
         <div>
-
+            <CareersHero />
         </div>
     )
 }
