a2545580940d7d446a5cf275ad71d01c55ecdb1d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-21 15:08:52 +0500

    Work Page Donr

--- src/app/(screens)/our-work/a-unified-brand-presence-for-a-unique-three-chamber-model/page.tsx
+++ src/app/(screens)/our-work/a-unified-brand-presence-for-a-unique-three-chamber-model/page.tsx
@@ -1,5 +1,6 @@
 import CareersHero from '@/components/careers/careers-hero'
 import ImpactSection from '@/components/our-work/impact-section'
+import PortfolioSection from '@/components/our-work/portfolio-section'
 import React from 'react'
 
 const UnifiedBrand = () => {
@@ -7,6 +8,7 @@ const UnifiedBrand = () => {
         <div>
             <CareersHero heading='A unified brand presence for a unique three-chamber model' highlight='presence' text='Dubai Chambers entrusted LPS as its strategic communications partner, recognizing the agency’s deep expertise in corporate communication strategies across the Middle East and its community-focused approach.​' image='single' />
             <ImpactSection />
+            <PortfolioSection />
         </div>
     )
 }
