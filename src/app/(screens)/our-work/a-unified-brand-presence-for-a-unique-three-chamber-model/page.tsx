30570df6f36c010482115acd0d49504b2511c009
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-21 13:19:55 +0500

    Hero Careers

--- src/app/(screens)/our-work/a-unified-brand-presence-for-a-unique-three-chamber-model/page.tsx
+++ src/app/(screens)/our-work/a-unified-brand-presence-for-a-unique-three-chamber-model/page.tsx
@@ -1,9 +1,10 @@
+import CareersHero from '@/components/careers/careers-hero'
 import React from 'react'
 
 const UnifiedBrand = () => {
     return (
         <div>
-            
+            <CareersHero />
         </div>
     )
 }
