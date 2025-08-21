9e7fa8713926c772c0a4a497f6f83b6d8d3a419f
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-21 13:52:20 +0500

    image

--- src/components/careers/career-image-grid.tsx
+++ src/components/careers/career-image-grid.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function CareersImageGrid({ image }: { image: "single" | "grid" }) {
   return (
     <div className="2xl:max-w-[1700px] w-[95%] mx-auto relative 2xl:top-72 top-52">
-      <div className="grid grid-cols-3 gap-4 ">
+      {image === "grid" ? <div className="grid grid-cols-3 gap-4 ">
         <div className="relative">
           <Image
             src={"/careers/careers-3.webp"}
@@ -32,7 +32,9 @@ function CareersImageGrid({ image }: { image: "single" | "grid" }) {
             className="rounded-tl-[70px]"
           />
         </div>
-      </div>
+      </div> : <div>
+
+      </div>}
     </div>
   );
 }
