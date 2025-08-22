bd095090b38c036fb18039998b72cb0487f82f46
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 11:42:00 +0500

    about us

--- src/app/(screens)/about-us/page.tsx
+++ src/app/(screens)/about-us/page.tsx
@@ -1,10 +1,10 @@
-import CareersHero from '@/components/careers/careers-hero'
+import AboutHero from '@/components/careers/careers-hero'
 import React from 'react'
 
 const AboutUs = () => {
     return (
         <div>
-            <CareersHero heading='Who we are' highlight='Who we are' text='LPS Brands is a 360 Impact Builder that uniquely blending new-age cool with at-scale execution for iconic brands across industries. We don’t just claim expertise—we are masters of both traditional and new-age marketing. Bringing the craftsmanship of Gen X with the execution power of Gen Z, we have evolved into a truly integrated agency, delivering marketing success stories that define the new age. The result? Turbo-charged growth and global recognition by driving real-world business impact. We are LPS, a 360° marketing agency built for modern platforms, the new breed of creators, and innovative formats.' bottom={false} />
+            <AboutHero heading='Who we are' highlight='Who we are' text='LPS Brands is a 360 Impact Builder that uniquely blending new-age cool with at-scale execution for iconic brands across industries. We don’t just claim expertise—we are masters of both traditional and new-age marketing. Bringing the craftsmanship of Gen X with the execution power of Gen Z, we have evolved into a truly integrated agency, delivering marketing success stories that define the new age. The result? Turbo-charged growth and global recognition by driving real-world business impact. We are LPS, a 360° marketing agency built for modern platforms, the new breed of creators, and innovative formats.' bottom={false} />
         </div>
     )
 }
