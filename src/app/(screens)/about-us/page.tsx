a535d32068a46563bde3ac039c1d3c70fc05a0f2
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 12:33:46 +0500

    Section 2

--- src/app/(screens)/about-us/page.tsx
+++ src/app/(screens)/about-us/page.tsx
@@ -1,3 +1,4 @@
+import ImageSection from '@/components/about/image-section'
 import AboutHero from '@/components/careers/careers-hero'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const AboutUs = () => {
     return (
         <div>
             <AboutHero heading='Who we are' highlight='Who we are' text='LPS Brands is a 360 Impact Builder that uniquely blending new-age cool with at-scale execution for iconic brands across industries. We don’t just claim expertise—we are masters of both traditional and new-age marketing. Bringing the craftsmanship of Gen X with the execution power of Gen Z, we have evolved into a truly integrated agency, delivering marketing success stories that define the new age. The result? Turbo-charged growth and global recognition by driving real-world business impact. We are LPS, a 360° marketing agency built for modern platforms, the new breed of creators, and innovative formats.' bottom={false} />
+            <ImageSection />
         </div>
     )
 }
