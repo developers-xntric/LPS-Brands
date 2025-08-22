a8820f3acbf993ba19ce69ec4bd5282ec8299434
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 13:07:29 +0500

    careers remove

--- src/app/(screens)/careers/page.tsx
+++ src/app/(screens)/careers/page.tsx
@@ -1,9 +1,7 @@
-import CareersImageGrid from "@/components/careers/career-image-grid";
 import CareersHero from "@/components/careers/careers-hero";
 import CareersSecond from "@/components/careers/careers-second";
 import { CareersThird } from "@/components/careers/careers-third";
 import { Button } from "@/components/common/button";
-import React from "react";
 
 function Careers() {
   return (
