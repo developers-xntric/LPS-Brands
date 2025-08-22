61a5bfdd4bb3dbdbe880998011c4c402470258a4
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 18:15:07 +0500

    hero section contact

--- src/app/(screens)/contact-us/page.tsx
+++ src/app/(screens)/contact-us/page.tsx
@@ -1,9 +1,10 @@
+import ContactHero from '@/components/contact/hero'
 import React from 'react'
 
 const Contact = () => {
     return (
         <div>
-
+            <ContactHero />
         </div>
     )
 }
