98986d4e4756851819b0c3fe570e300aafc05305
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 14:40:21 +0500

    meet the pears

--- src/app/(screens)/about-us/page.tsx
+++ src/app/(screens)/about-us/page.tsx
@@ -1,6 +1,7 @@
 import { AboutLPS } from '@/components/about/about-lps'
 import ImageSection from '@/components/about/image-section'
 import AboutHero from '@/components/careers/careers-hero'
+import { MeetThePears } from '@/components/home/meet-the-pears'
 import React from 'react'
 
 const AboutUs = () => {
@@ -11,6 +12,7 @@ const AboutUs = () => {
             </p>' bottom={false} />
             <ImageSection />
             <AboutLPS />
+            <MeetThePears />
         </div>
     )
 }
