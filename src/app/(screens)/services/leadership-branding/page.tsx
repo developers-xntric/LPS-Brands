a9cb4364c84cdf85ac7f09157059e1869395a876
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 11:43:47 +0500

    Leadership

--- src/app/(screens)/services/leadership-branding/page.tsx
+++ src/app/(screens)/services/leadership-branding/page.tsx
@@ -1,4 +1,4 @@
-import CareersHero from '@/components/careers/careers-hero';
+import ServiceHeo from '@/components/careers/careers-hero';
 import ContactFormSection from '@/components/services/form';
 import ProductionEvolutionSection from '@/components/services/production-evolution-section';
 import React from 'react'
@@ -6,7 +6,7 @@ import React from 'react'
 const LeadershipBranding = () => {
     return (
         <div>
-            <CareersHero heading='LEADERSHIP BRANDING' highlight='LEADERSHIP BRANDING' bottom={false} />
+            <ServiceHeo heading='LEADERSHIP BRANDING' highlight='LEADERSHIP BRANDING' bottom={false} />
             <ProductionEvolutionSection />
             <ContactFormSection />
         </div>
