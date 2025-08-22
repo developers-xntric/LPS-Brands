0752a8f4a10c4d38601af130de9f2a3119da45a1
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-22 18:55:27 +0500

    Contact Completed

--- src/app/(screens)/contact-us/page.tsx
+++ src/app/(screens)/contact-us/page.tsx
@@ -1,3 +1,4 @@
+import ContactEmails from '@/components/contact/contact-emails'
 import ContactHero from '@/components/contact/hero'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Contact = () => {
     return (
         <div>
             <ContactHero />
+            <ContactEmails />
         </div>
     )
 }
