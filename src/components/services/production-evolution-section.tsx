60f02b4278f93de3d486927b3a18ce4a5ed730fb
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-25 14:02:14 +0500

    Images array

--- src/components/services/production-evolution-section.tsx
+++ src/components/services/production-evolution-section.tsx
@@ -3,9 +3,11 @@ import Wrapper from "../layout/wrapper"
 interface HeadingProps {
     title?: string;
     highlight?: string;
-    heading?: string
+    heading?: string;
+    text?: string[];
+    images?: string[]
 }
-export default function ProductionEvolutionSection({ title, highlight, heading }: HeadingProps) {
+export default function ProductionEvolutionSection({ title, highlight, heading, text, images }: HeadingProps) {
 
     function getHighlightedText(title: string, highlight: string) {
         const parts = title.split(new RegExp(`(${highlight})`, "gi"));
@@ -44,7 +46,7 @@ export default function ProductionEvolutionSection({ title, highlight, heading }
                     <div className="relative">
                         <div className="relative z-10 p-8 rounded-lg shadow-sm top-14">
                             <p className="text-secondary leading-7 font-medium text-lg font-['Exo']">
-                                Leadership branding is the cornerstone of how you’re seen, appreciated, and followed in the competitive environment of today, not simply a slogan. Whether your role is changemaker, entrepreneur, or executive, your own leadership brand determines your effect. Establishing a leadership brand requires establishing your beliefs, approach of communication, and special presence you provide to every room.
+                                {(text && text[0]) ? text[0] : "Leadership branding is the cornerstone of how you’re seen, appreciated, and followed in the competitive environment of today, not simply a slogan. Whether your role is changemaker, entrepreneur, or executive, your own leadership brand determines your effect. Establishing a leadership brand requires establishing your beliefs, approach of communication, and special presence you provide to every room."}
                             </p>
                         </div>
                     </div>
@@ -53,7 +55,7 @@ export default function ProductionEvolutionSection({ title, highlight, heading }
                     <div className="relative">
                         <div className="rounded-lg shadow-sm overflow-hidden">
                             <Image
-                                src="/services/pe1.webp"
+                                src={(images && images[0]) ? images[0] : "/services/pe1.webp"}
                                 alt="Luxury jewelry packaging display"
                                 width={600}
                                 height={400}
@@ -69,7 +71,7 @@ export default function ProductionEvolutionSection({ title, highlight, heading }
                     <div className="relative">
                         <div className="bg-white shadow-sm overflow-hidden">
                             <Image
-                                src="/services/pe2.webp"
+                                src={(images && images[1]) ? images[1] : "/services/pe2.webp"}
                                 alt="Digital display in urban setting"
                                 width={500}
                                 height={300}
@@ -82,7 +84,7 @@ export default function ProductionEvolutionSection({ title, highlight, heading }
                     <div className="relative">
                         <div className="p-8 shadow-sm">
                             <p className="text-secondary font-medium leading-7 text-lg font-['Exo']">
-                                This goes beyond just leadership. In a society where authority is acquired by authenticity, cultural design for impact, relevance, and influence follows. A leadership brand is the junction between your view of yourself and your professional identity. It’s about regularly and honestly presenting your leadership style, beliefs, strengths, and purpose.
+                                {(text && text[1]) ? text[1] : "This goes beyond just leadership. In a society where authority is acquired by authenticity, cultural design for impact, relevance, and influence follows. A leadership brand is the junction between your view of yourself and your professional identity. It’s about regularly and honestly presenting your leadership style, beliefs, strengths, and purpose."}
                             </p>
                         </div>
                     </div>
