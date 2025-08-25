ada8a1f9ad146169a4d4d8f4fb9f8fd460fd613a
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-25 13:42:26 +0500

    corporate branding

---
+++ src/app/(screens)/services/corporate-branding/page.tsx
@@ -0,0 +1,55 @@
+import CareersHero from '@/components/careers/careers-hero';
+import BrandingStart from '@/components/services/branding-start';
+import BuildingBrandsCards from '@/components/services/building-brands-cards';
+import DisplayFeatured from '@/components/services/display-featured';
+import ContactFormSection from '@/components/services/form';
+import ProductionEvolutionSection from '@/components/services/production-evolution-section';
+import Timeline from '@/components/services/time-line';
+import Script from 'next/script';
+import React from 'react'
+
+const schemaData = {
+    "@context": "https://schema.org",
+    "@type": "Service",
+    "name": "Corporate Branding",
+    "description": "Purpose-Driven Corporate Branding That Builds More Than Identity.",
+    "provider": {
+        "@type": "Organization",
+        "name": "LPS Brands",
+        "url": "https://lps-me.com"
+    },
+    "serviceType": "Corporate Branding",
+    "areaServed": {
+        "@type": "Place",
+        "name": "Global"
+    },
+    "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.8",
+        "reviewCount": "53",
+        "bestRating": "7",
+        "worstRating": "1"
+    }
+}
+
+const CorporateBranding = () => {
+    return (
+        <div>
+            <Script
+                id="schema-service-cloud"
+                type="application/ld+json"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+            />
+            <CareersHero heading='Corporate Branding' highlight='Leadership Branding' bottom={false} />
+            <ProductionEvolutionSection />
+            <Timeline />
+            <BuildingBrandsCards />
+            <BrandingStart />
+            <DisplayFeatured />
+            <ContactFormSection />
+        </div>
+    )
+}
+
+export default CorporateBranding;
