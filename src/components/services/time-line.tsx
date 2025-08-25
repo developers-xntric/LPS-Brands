bc8622e4cf06c4dc7fc3a20aabe057c530c1d172
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-25 12:46:52 +0500

    index removed

--- src/components/services/time-line.tsx
+++ src/components/services/time-line.tsx
@@ -60,7 +60,7 @@ export default function Timeline() {
         }
       );
 
-      itemsRef.current.forEach((item, index) => {
+      itemsRef.current.forEach((item) => {
         if (!item) return;
 
         const circle = item.querySelector(".timeline-circle");
