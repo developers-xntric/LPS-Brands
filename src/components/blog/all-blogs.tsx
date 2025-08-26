f8eef3c14469848c69520af1e0c69f67dc255dc8
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-26 11:17:59 +0500

    Production Ready

--- src/components/blog/all-blogs.tsx
+++ src/components/blog/all-blogs.tsx
@@ -1,11 +1,10 @@
 "use client";
+import { Calendar } from "lucide-react";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Head from "next/head";
-import axios from "axios";
 import { Button } from "../common/button";
-import { Calendar } from "lucide-react";
 
 interface Blog {
   id: number;
