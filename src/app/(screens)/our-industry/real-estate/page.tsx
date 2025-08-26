a9cd595ab3287cdd60d3a79d0d782dbe2c2032a9
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-26 17:24:42 +0500

    real estate

--- src/app/(screens)/our-industry/real-estate/page.tsx
+++ src/app/(screens)/our-industry/real-estate/page.tsx
@@ -1,20 +1,45 @@
-import CareersHero from '@/components/careers/careers-hero'
-import FirstSection from '@/components/our-industry/first-section'
-import FourSection from '@/components/our-industry/four-section'
-import SecondSection from '@/components/our-industry/second-section'
-import ThirdSection from '@/components/our-industry/third-section'
-import React from 'react'
+import CareersHero from "@/components/careers/careers-hero";
+import FirstSection from "@/components/our-industry/first-section";
+import FourSection from "@/components/our-industry/four-section";
+import SecondSection from "@/components/our-industry/second-section";
+import ThirdSection from "@/components/our-industry/third-section";
+import React from "react";
 
-const FinancialServices = () => {
+const RealState = () => {
   return (
     <div>
-        <CareersHero heading='Government' text='We communicate policy and craft public trust.  At LPS, we help government institutions connect with citizens, modernize engagement, and inspire collective progress.' highlight='Government' bottom={false} />
-        <FirstSection/>
-        <SecondSection/>
-        <ThirdSection/>
-        <FourSection/>
+      <CareersHero
+        heading="Real Estate"
+        text="Innovative solutions tailored to elevate modern living and redefine property experiences across every development."
+        highlight="Real Estate"
+        bottom={false}
+      />
+      <FirstSection
+        cardHead1="Industry Specific Challenges"
+        cardPara1="Dubai’s real estate industry faces several key challenges that hinder its long-term growth. Market oversupply continues to be a pressing issue, often leading to price instability and extended property turnover times. The lack of digital integration across platforms slows down processes and reduces overall efficiency. Transparency concerns, particularly around pricing and regulatory clarity, can deter both local and international investors. Additionally, the customer journey is often fragmented, with inconsistent service across agents and developers. These challenges highlight the urgent need for technology-driven solutions and improved regulatory frameworks to enhance trust and streamline experiences."
+        cardHead2="Evolving Challenges in a Digitally-Driven Market"
+        cardPara2="Dubai’s real estate industry faces several key challenges that hinder its long-term growth. Market oversupply continues to be a pressing issue, often leading to price instability and extended property turnover times. The lack of digital integration across platforms slows down processes and reduces overall efficiency. Transparency concerns, particularly around pricing and regulatory clarity, can deter both local and international investors. Additionally, the customer journey is often fragmented, with inconsistent service across agents and developers. These challenges highlight the urgent need for technology-driven solutions and improved regulatory frameworks to enhance trust and streamline experiences."
+        cardImg1="/our-industry/c1.webp"
+        cardImg2="/our-industry/c2.webp"
+        firstImg="/our-industry/1.webp"
+        head1="What are the trends of Real Estate industry."
+        para1="Real estate in Dubai is rapidly evolving with smart technologies, AI-driven analytics, and digital platforms transforming property management and investment. The shift toward sustainable, tech-enabled developments is redefining urban living and investor experiences."
+      />
+      <SecondSection />
+      <ThirdSection
+        img="/our-industry/t1.webp"
+        heading="How LPS helps real estate industry"
+        para1="LPS provides a comprehensive range of digital solutions designed to address the key challenges in Dubai's real estate sector. Centralized platforms, CRMs, and automated systems streamline property sales, client management, and communication, eliminating inefficiencies caused by outdated processes. Immersive AR/VR experiences and interactive property visualizations enhance customer engagement, enabling potential buyers and investors to explore properties in-depth without physical visits."
+        para2="Transparency is strengthened through real-time data access, giving buyers and investors accurate updates on property details and progress. A tech-driven approach modernizes operations, boosts scalability, and delivers personalized experiences that build lasting client relationships."
+      />
+      <FourSection
+        bgImage="/our-industry/41.webp"
+        heading="Evolving Challenges in a Digitally-Driven Market"
+        para1="LPS demonstrates a nuanced understanding of the real estate industry’s market shifts by closely monitoring regional trends and adapting to evolving demands. The Middle East’s real estate sector is experiencing significant growth, with projections indicating a market value of USD 2,264.89 billion by 2030."
+        para2="LPS aligns its strategies with these developments, focusing on emerging opportunities in residential, commercial, and luxury real estate segments to meet the dynamic needs of the market."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default FinancialServices
+export default RealState;
