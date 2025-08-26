c110e05d4e3afa2ec9684265bd2768bb2430a77d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-26 13:32:42 +0500

    influencers

--- src/app/(screens)/services/influencers/page.tsx
+++ src/app/(screens)/services/influencers/page.tsx
@@ -33,7 +33,7 @@ const schemaData = {
     }
 }
 
-const CorporateBranding = () => {
+const Influencers = () => {
     return (
         <div>
             <Script
@@ -106,4 +106,4 @@ const CorporateBranding = () => {
     )
 }
 
-export default CorporateBranding;
+export default Influencers;
