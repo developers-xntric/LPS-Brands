8d5a9d617d2b42e1c5bf895fa9643b5995ee1e2a
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-08-27 13:22:10 +0500

    werf

--- src/components/careers/careers-hero.tsx
+++ src/components/careers/careers-hero.tsx
@@ -53,7 +53,7 @@ function CareersHero({
     if (highlight && heading.includes(highlight)) {
       const parts = heading.split(new RegExp(`(${highlight})`, "gi"));
       return (
-        <h1 className="2xl:text-[75px] text-[40px] leading-[40px] font-bold uppercase max-w-[70%] 2xl:leading-[80px]">
+        <h1 className="lg:text-[75px] text-[40px] leading-[40px] font-bold uppercase max-w-[70%] 2xl:leading-[80px]">
           {parts.map((part, i) =>
             part.toLowerCase() === highlight.toLowerCase() ? (
               <span key={i} className="gradient-text">
