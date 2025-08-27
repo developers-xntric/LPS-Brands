d8eb08d973c676e5a258219b1d2920f47090c8c4
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-08-26 22:29:36 -1200

    fsdfsd

--- src/components/services/display-featured.tsx
+++ src/components/services/display-featured.tsx
@@ -7,19 +7,71 @@ import { Button } from "../common/button";
 interface PortfolioItem {
   id: number;
   frontImage: string;
+  title: string;
+  para: string;
 }
 
 const portfolioItems: PortfolioItem[] = [
-  { id: 1, frontImage: "/services/flip-card-bg.webp" },
-  { id: 2, frontImage: "/services/flip-card-bg.webp" },
-  { id: 3, frontImage: "/services/flip-card-bg.webp" },
-  { id: 4, frontImage: "/services/flip-card-bg.webp" },
-  { id: 5, frontImage: "/services/flip-card-bg.webp" },
-  { id: 6, frontImage: "/services/flip-card-bg.webp" },
-  { id: 7, frontImage: "/services/flip-card-bg.webp" },
-  { id: 8, frontImage: "/services/flip-card-bg.webp" },
-  { id: 8, frontImage: "/services/flip-card-bg.webp" },
-  { id: 8, frontImage: "/services/flip-card-bg.webp" },
+  {
+    id: 1,
+    frontImage: "/work/1.webp",
+    title: "A unified brand presence for a unique three-chamber model",
+    para: "Recognizing our expertise in corporate communication strategies, Dubai Chambers partnered with us as a strategic communications agency to strengthen its community-focused initiatives across the Middle East.",
+  },
+  {
+    id: 2,
+    frontImage: "/work/2.webp",
+    title: "Dubai Economy & Tourism",
+    para: "We were entrusted as Social Video Partners for Visit Dubai, helping to create a unified brand presence and engaging video content that highlighted Dubai’s unique experiences.",
+  },
+  {
+    id: 3,
+    frontImage: "/work/7.webp",
+    title: "Coffee Planet",
+    para: "As a homegrown specialty coffee brand in the UAE, Coffee Planet wanted to change consumer perceptions and establish itself as the top choice for coffee lovers in the country. The challenge was to reshape brand messaging while maintaining authenticity.",
+  },
+  {
+    id: 4,
+    frontImage: "/work/8.webp",
+    title: "Delmonte",
+    para: "Del Monte aimed to enhance its product visibility across the MENA region, focusing on generating brand awareness and increasing foot traffic to physical retail stores.",
+  },
+  {
+    id: 5,
+    frontImage: "/work/6.webp",
+    title: "Create Apps by DC",
+    para: "Dubai Chamber of Digital Economy launched the \"Create Apps in Dubai\" initiative to establish itself as a leader in digital innovation. The goal was to create a unified brand presence to position Dubai as a global tech hub.",
+  },
+  {
+    id: 6,
+    frontImage: "/work/5.webp",
+    title: "London Dairy",
+    para: "Premium ice cream brand London Dairy wanted to promote its three new flavors while increasing brand recognition. Our team developed a targeted campaign to drive engagement and sales.",
+  },
+  {
+    id: 7,
+    frontImage: "/work/4.webp",
+    title: "Mentos",
+    para: "Mentos sought to create a multinational awareness campaign linking their brand to the FIFA World Cup—without incurring sponsorship costs. The goal was to associate Mentos with the excitement of the global event.",
+  },
+  {
+    id: 8,
+    frontImage: "/work/3.webp",
+    title: "Dolby",
+    para: "As a long-term client, Dolby wanted to explore the potential of in-stream gaming advertisements to boost brand awareness. The challenge was to create a strategic campaign that leveraged gaming platforms effectively.",
+  },
+  {
+    id: 9,
+    frontImage: "/work/9.webp",
+    title: "Skechers- Hajj Campain",
+    para: "We collaborated with Skechers to create a culturally insightful film specifically for their Hajj campaign. This project aimed to connect deeply with the Arabic audience by focusing on culturally relevant storytelling and visuals.",
+  },
+  {
+    id: 10,
+    frontImage: "/work/10.webp",
+    title: "Skechers- BTS",
+    para: "Skechers aimed to create a visually compelling video that not only told a great story but also showcased the brand’s wide range of designs in a captivating way.",
+  },
 ];
 
 export default function DisplayFeatured({ text }: { text?: string }) {
@@ -46,7 +98,8 @@ export default function DisplayFeatured({ text }: { text?: string }) {
               A Display of Our Work
             </h2>
             <p className="text-white font-medium leading-7 mb-10 lg:mb-0 text-md font-['Exo'] max-w-4xl mx-auto">
-              {text || "Our impact speaks through the leaders we’ve shaped. From Fortune 500 executives to disruptive founders, public figures to policy shapers, our leadership branding work has turned personal stories into powerful movements."}
+              {text ||
+                "Our impact speaks through the leaders we’ve shaped. From Fortune 500 executives to disruptive founders, public figures to policy shapers, our leadership branding work has turned personal stories into powerful movements."}
             </p>
           </div>
           <Button text="View All Work" center={true} />
@@ -57,9 +110,8 @@ export default function DisplayFeatured({ text }: { text?: string }) {
           <div
             className="flex transition-transform duration-700 ease-in-out"
             style={{
-
               width: `${(totalItems * 70) / cardsPerView}%`,
-              transform: `translateX(-${(currentIndex *   100) / cardsPerView}%)`,
+              transform: `translateX(-${(currentIndex * 100) / cardsPerView}%)`,
             }}
           >
             {portfolioItems.map((item) => (
@@ -108,17 +160,16 @@ function FlipCard({ item }: FlipCardProps) {
         </div>
 
         {/* Back */}
-        <div className="absolute top-[-23px] inset-0 w-full h-80 flex items-center rounded-3xl overflow-hidden bg-[#c5c5c5] [backface-visibility:hidden]"
-          style={{ transform: "rotateY(180deg)" }}>
+        <div
+          className="absolute top-[-23px] inset-0 w-full h-80 flex items-center rounded-3xl overflow-hidden bg-[#c5c5c5] [backface-visibility:hidden]"
+          style={{ transform: "rotateY(180deg)" }}
+        >
           <div className="p-6 flex flex-col items-center justify-center">
-            <h3 className="text-[26px] font-bold text-secondary mb-2">
-              Skechers - Hajj Campaign
+            <h3 className="text-[26px] font-bold text-secondary mb-4 lg:max-w-[90%] leading-[26px] mx-auto">
+              {item.title}
             </h3>
             <p className="text-secondary font-medium text-center leading-2 text-sm font-['Exo'] max-w-4xl mx-auto">
-              We collaborated with Skechers to create a culturally insightful
-              film specifically for their Hajj campaign. This project aimed to
-              connect deeply with the Arabic audience by focusing on culturally
-              relevant storytelling and visuals.
+              {item.para}
             </p>
           </div>
         </div>
