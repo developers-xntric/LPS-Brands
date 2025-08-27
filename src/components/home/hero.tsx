685d4aceb982822156a1685161504f1fb96aad0e
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-27 11:54:35 +0500

    hero

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
     <div className="bg-[url('/home/Hero.png') bg-center bg-cover ">
       <div className="2xl:max-w-[1800px] mx-auto w-[95%] pb-20">
         {/* Top Left LPS Logo */}
-        <div className="p-4 ms-6">
+        <div className="hidden lg:block p-4 ms-6">
           <LogoLPS />
         </div>
         {/* Center Animated Heading of Pears In a World Of Apples */}
