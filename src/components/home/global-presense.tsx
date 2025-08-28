96ce022b4c8753d4f7b9aef917afe07a0229d33d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-08-28 16:41:46 +0500

    update

--- src/components/home/global-presense.tsx
+++ src/components/home/global-presense.tsx
@@ -10,7 +10,7 @@ const GlobalPresence = () => {
     return (
         <Wrapper>
             <div className="flex flex-col xl:flex-row gap-10 xl:justify-between py-16 relative">
-                <div className="relative md:h-[630px] lg:h-[760px] xl:w-[65%] bg-[url('/home/bg-grad.png')] bg-cover bg-center rounded-3xl md:rounded-[50px]">
+                <div className="relative h-[870px] md:h-[630px] lg:h-[760px] xl:w-[65%] bg-[url('/home/bg-grad.png')] bg-cover bg-center rounded-3xl md:rounded-[50px]">
                     <HoverImage
                         defaultSrc="/home/zig-zag.png"
                         hoverSrc="/home/zig-zag.png"
