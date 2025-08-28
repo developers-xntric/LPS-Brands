7dbdcecd0cc49d6e0bc912154d6e44a7ed2f5dfd
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-08-28 15:55:24 +0500

    dwe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,41 @@ import HeroSection from "@/components/home/hero";
 import Industries from "@/components/home/industries";
 import { MeetThePears } from "@/components/home/meet-the-pears";
 import Wrapper from "@/components/layout/wrapper";
+import Script from "next/script";
 
 export default function Home() {
+  const schemaData = {
+    "@context": "https://schema.org",
+    "@type": "Service",
+    name: "Website & App",
+    description: "Website Design and Development Services",
+    provider: {
+      "@type": "Organization",
+      name: "LPS Brands",
+      url: "https://lps-me.com",
+    },
+    serviceType: "Website & App",
+    areaServed: {
+      "@type": "Place",
+      name: "Global",
+    },
+    aggregateRating: {
+      "@type": "AggregateRating",
+      ratingValue: "4.8",
+      reviewCount: "53",
+      bestRating: "7",
+      worstRating: "1",
+    },
+  };
+
   return (
     <div>
+      <Script
+        id="schema-service-cloud"
+        type="application/ld+json"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+      />
       <HeroSection />
       <FeaturedWorks />
       <GlobalPresence />
