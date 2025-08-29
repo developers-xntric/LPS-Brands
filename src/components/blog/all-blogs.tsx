5d716ed090485f44d0232c9927725492b2ce0c48
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-08-29 13:25:02 +0500

    wdq

--- src/components/blog/all-blogs.tsx
+++ src/components/blog/all-blogs.tsx
@@ -175,7 +175,7 @@ const Cards = () => {
                   <div key={index}>
                     <div className="bg-white rounded-2xl overflow-hidden duration-300">
                       <Image
-                        src={`https://lps-me.com/${card.imageURL} || "/placeholder.svg"`}
+                        src={card.imageURL || "/placeholder.svg"}
                         width={400}
                         height={250}
                         alt={card.title}
