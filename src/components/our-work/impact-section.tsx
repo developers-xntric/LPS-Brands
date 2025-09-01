3d410de3b00e9066cb674ad02da91ab18363694f
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-09-01 01:05:53 -1200

    dsfdf

--- src/components/our-work/impact-section.tsx
+++ src/components/our-work/impact-section.tsx
@@ -88,7 +88,7 @@ export default function ImpactSection({
 
           {/* Statistics Grid */}
         </div>
-        <div className=" grid-rows-2 grid grid-cols-2 lg:place-self-end lg:gap-7 lg:w-1/2">
+        <div className="grid grid-cols-2 lg:place-self-end lg:gap-7 lg:w-1/2">
           {numbersData &&
             numbersData.map((item, index) => {
               return (
