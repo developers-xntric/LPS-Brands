3b44c2012d6d1060a0e238c3aa9a172ff9bfef95
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-09-01 18:17:47 +0500

    webite-app

--- src/app/(screens)/services/website-app/page.tsx
+++ src/app/(screens)/services/website-app/page.tsx
@@ -8,6 +8,15 @@ import ProductionEvolutionSection from "@/components/services/production-evoluti
 import Timeline from "@/components/services/time-line";
 import Script from "next/script";
 
+export const metadata = {
+    title: "Website Design and Development Services in Dubai, UAE | LPS",
+    description:
+        "Craft powerful websites &amp; apps. We offer website design and development services that drive performance, scalability &amp; user engagement.",
+    alternates: {
+        canonical: "https://lps-me.com/services/website-app/",
+    },
+};
+
 const schemaData = {
   "@context": "https://schema.org",
   "@type": "Service",
