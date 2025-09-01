84c13c00cb4788406656a06abd3e028ce02c462c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-09-01 12:12:40 +0500

    blog-section

--- src/components/home/blog-section.tsx
+++ src/components/home/blog-section.tsx
@@ -15,11 +15,13 @@ export default function BlogsSection({
     <div>
       <div className="py-16 mx-auto bg-[url('/home/blog-bg.png')] rounded-3xl bg-center bg-cover bg-no-repeat">
         {/* Header */}
-        <div className="flex flex-col gap-4 md:flex-row px-12 items-center justify-between mb-12">
+        <div className="flex flex-col gap-4 md:flex-row px-4 md:px-12 md:items-center md:justify-between mb-12">
           <h2 className="text-4xl font-bold text-white">
             {heading ? heading : "Blogs"}
           </h2>
-          <Button text="View All Blogs" link="/blog" />
+          <div className="flex flex-start md:flex-end">
+            <Button text="View All Blogs" link="/blog" />
+          </div>
         </div>
 
         {/* Blog Carousel */}
