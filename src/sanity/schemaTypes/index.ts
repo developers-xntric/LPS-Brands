c56e0413c7e72aceab06c91f8f904925ae8d6f1b
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-14 11:38:56 +0500

    blog type added

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { LPSBlogType } from './PostType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [LPSBlogType],
 }
